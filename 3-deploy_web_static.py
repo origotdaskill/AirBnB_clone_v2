c38630e3e326e037aa33135e1f0dbabd790eeedc
origotdaskill <mr.walid.ori@gmaill.com>
2024-05-02 17:23:03 +0100

    ori commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['52.201.219.251', '54.144.147.139']
 
 
 @runs_once
