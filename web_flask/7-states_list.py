1298a8d73c11c9aaee7335db5007e679a0c145e2
origotdaskill <mr.walid.ori@gmaill.com>
2024-05-18 23:37:28 +0100

    ori commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-    
-"""
+"""Starts a Flask web application"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """Displays a HTML page with a list of all State objects in DBStorage."""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_appcontext(exc):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
